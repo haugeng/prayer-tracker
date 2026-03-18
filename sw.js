1564f045fd36236777da63efaa07ba17f3898bc4
HauGeng <42998455+haugeng@users.noreply.github.com>
2026-03-18 23:14:25 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,22 @@
-const cacheName = 'prayer-tracker-v4.0.3';
-// GitHub Pages paths are relative to the repository root
+const cacheName = 'prayer-tracker-v4.0.4';
 const assets = [
-  './',          // The current directory
-  'index.html',  // The specific file
-  'sw.js'        // Cache the worker itself
+  './',
+  'index.html',
+  'sw.js'
 ];
 
 self.addEventListener('install', e => {
     self.skipWaiting();
-    e.waitUntil(
-        caches.open(cacheName).then(cache => cache.addAll(assets))
-    );
+    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('activate', e => {
     self.clients.claim();
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(k => k !== cacheName ? caches.delete(k) : null)
-        ))
-    );
+    e.waitUntil(caches.keys().then(keys => Promise.all(
+        keys.map(k => k !== cacheName ? caches.delete(k) : null)
+    )));
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
